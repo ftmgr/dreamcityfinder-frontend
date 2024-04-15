f334ed05685192a42f1794cee5ba279c3c0e6199
Fabio Niglio <fabio_niglio@hotmail.com>
2024-04-15 17:15:38 +0200

    fix footer

--- src/pages/ContentPage.jsx
+++ src/pages/ContentPage.jsx
@@ -1,8 +1,16 @@
 import videoBg from "../assets/videoBg.mp4";
 import classes from "../styles/Content.module.css";
 import cityData from "../assets/cities.json";
+import { useState } from "react";
+import { Input } from "@mantine/core";
 
 const ContentPage = () => {
+  const [searchTerm, setSearchTerm] = useState(""); // State to hold the search term
+
+  // Filter cities based on search term
+  const filteredCities = cityData.cities.filter((city) =>
+    city.cityname.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <>
       <div className={classes.container}>
@@ -13,8 +21,16 @@ const ContentPage = () => {
           muted
           className={classes.video}
         ></video>
+        <Input
+          radius="md"
+          placeholder="Search for cities..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className={classes.searchInput}
+        />
+
         <div className={classes.cityGrid}>
-          {cityData.cities.map((city, index) => (
+          {filteredCities.map((city) => (
             <div key={city.id} className={classes.cityBlock}>
               <img
                 src={city.picture[0].src}
