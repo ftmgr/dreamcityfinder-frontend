000a08e5c91bdc7c2f1bc1aee691522522b33e54
Fabio Niglio <fabio_niglio@hotmail.com>
2024-04-15 17:03:32 +0200

    content page video and list of cities

--- src/pages/ContentPage.jsx
+++ src/pages/ContentPage.jsx
@@ -1,8 +1,29 @@
+import videoBg from "../assets/videoBg.mp4";
+import classes from "../styles/Content.module.css";
+import cityData from "../assets/cities.json";
+
 const ContentPage = () => {
   return (
     <>
-      <div className="container">
-        <p>This is content Page!</p>
+      <div className={classes.container}>
+        <video
+          src={videoBg}
+          autoPlay
+          loop
+          muted
+          className={classes.video}
+        ></video>
+        <div className={classes.cityGrid}>
+          {cityData.cities.map((city, index) => (
+            <div key={city.id} className={classes.cityBlock}>
+              <img
+                src={city.picture[0].src}
+                alt={`Image of ${city.cityname}`}
+              />
+              <div className={classes.cityName}>{city.cityname}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
