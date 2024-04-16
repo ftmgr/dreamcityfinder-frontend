c79d750ac8adbaee1c6677b0b917e7e1df17921b
Fabio Niglio <fabio_niglio@hotmail.com>
2024-04-16 12:21:40 +0200

    adding card info city

--- src/pages/CityDetailPage.jsx
+++ src/pages/CityDetailPage.jsx
@@ -1,41 +1,63 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { Button, Card, Image, Text } from "@mantine/core";
 import cityData from "../assets/cities.json";
 
 const CityDetailPage = () => {
-  const [city, setCity] = useState(cityData[0]);
-
-  // React Router hook for navigation. We use it for the back button. You can leave this as it is.
+  const [city, setCity] = useState(null); // Initialize city as null
+  const { cityId } = useParams(); // Get the cityId from the URL
   const navigate = useNavigate();
-  //const { cityId } = useParams();
+
+  useEffect(() => {
+    // Find the city that matches the cityId from the URL
+    const foundCity = cityData.cities.find(
+      (city) => city.id.toString() === cityId
+    );
+    if (foundCity) {
+      setCity(foundCity);
+    } else {
+      // Handle case where no city is found
+      console.log("No city found with id:", cityId);
+    }
+  }, [cityId]);
+
+  if (!city) {
+    return <Text>No city data available.</Text>;
+  }
 
   return (
-    <div className="d-inline-flex flex-column justify-content-center align-items-center w-100 p-4">
-      {city && (
-        <>
-          <img
-            src={city.picture[0].src}
-            alt="City Image"
-            height="300px"
-            width="auto"
-          />
-          <h3>{city.cityname}</h3>
-          <p>{city.avgScore}</p>
-          <p>Country: {city.country}</p>
-          <p>Continent: {city.continent}</p>
-          <p>Cost of Living: {city.costOfLiving}</p>
+    <Card
+      shadow="sm"
+      padding="lg"
+      style={{ maxWidth: 400, margin: "2rem auto", marginTop: "100px" }}
+    >
+      <Card.Section>
+        <Image
+          src={city.picture[0].src}
+          height={160}
+          alt={`Image of ${city.cityname}`}
+          fit="cover"
+        />
+      </Card.Section>
 
-          <button
-            className="btn btn-primary"
-            onClick={() => {
-              navigate(-1);
-            }}
-          >
-            Back
-          </button>
-        </>
-      )}
-    </div>
+      <Text weight={500} size="lg" mt="md">
+        {city.cityname}
+      </Text>
+
+      <Text size="sm">Average Score: {city.avgScore}</Text>
+      <Text size="sm">Country: {city.country}</Text>
+      <Text size="sm">Continent: {city.continent}</Text>
+      <Text size="sm">Cost of Living: {city.costOfLiving}</Text>
+
+      <Button
+        variant="outline"
+        color="blue"
+        mt="md"
+        onClick={() => navigate(-1)}
+      >
+        Back
+      </Button>
+    </Card>
   );
 };
 
