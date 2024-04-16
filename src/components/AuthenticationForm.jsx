cf76c51dd0ece775e8b80b618bb49ad6b558658d
Fatma <fatmagurses1@gmail.com>
2024-04-16 15:02:15 +0200

    Authorization Form

--- src/components/AuthenticationForm.jsx
+++ src/components/AuthenticationForm.jsx
@@ -1,5 +1,12 @@
 import { useToggle, upperFirst } from '@mantine/hooks';
 import { useForm } from '@mantine/form';
+import api from './registerUser';
+import { useState } from 'react';
+import axios from 'axios';
+import JSON from '../db.json';
+
+//import { useHistory } from 'react-router-dom';
+
 import {
     TextInput,
     PasswordInput,
@@ -11,16 +18,24 @@ import {
     Checkbox,
     Anchor,
     Stack,
+    Flex,
 } from '@mantine/core';
+
 
 export function AuthenticationForm(props) {
     const [type, toggle] = useToggle(['login', 'register']);
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [terms, setTerms] = useState(false);
+
+
     const form = useForm({
         initialValues: {
             email: '',
             name: '',
             password: '',
-            terms: true,
+            terms: false,
         },
 
         validate: {
@@ -29,14 +44,63 @@ export function AuthenticationForm(props) {
         },
     });
 
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    const API_BASE_URL = 'http://localhost:4000';
+
+    //http://localhost:4000/users
+
+    const api = axios.create({
+        baseURL: API_BASE_URL
+    });
+
+    const registerUser = async (userData) => {
+
+        try {
+            const response = await api.post('/register', userData);
+            return response.data;
+        } catch (error) {
+            throw error.response.data;
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const { name, email, password } = e.target;
+            const newUser = { ...formData };
+
+            setFormData({
+                ...formData,
+                name: name,
+                email: email,
+                password: password,
+            });
+
+            const response = await registerUser(formData);
+            if (response.status === 'success') {
+                //history.push('/profile');
+                console.log("Sucess to register a new user!")
+            }
+
+        } catch (error) {
+            console.error('Registration failed:', error);
+        }
+    };
+
     return (
-        <Paper radius="md" p="xl" withBorder {...props}>
+        <Paper radius="md" p="xl" style={{ width: "400px", align: "center" }} withBorder {...props}>
             <Text size="lg" fw={500}>
-                Welcome! {type}
+                Welcome! {type === 'login' ? <p>Login</p> : <p>Register</p>}
 
             </Text>
 
-            <form onSubmit={form.onSubmit(() => { })}>
+            <form onSubmit={(e) => handleSubmit(e)}>
                 <Stack>
                     {type === 'register' && (
                         <TextInput
@@ -83,7 +147,7 @@ export function AuthenticationForm(props) {
                             ? 'Already have an account? Login'
                             : "Don't have an account? Register"}
                     </Anchor>
-                    <Button type="submit" radius="xl">
+                    <Button type="submit" radius="xl" >
                         {upperFirst(type)}
                     </Button>
                 </Group>
