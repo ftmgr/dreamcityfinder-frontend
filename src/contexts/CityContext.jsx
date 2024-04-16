02e2eb868a288f633f92b23c4287a5b456f49d29
Fabio Niglio <fabio_niglio@hotmail.com>
2024-04-16 14:52:06 +0200

    using env

--- src/contexts/CityContext.jsx
+++ src/contexts/CityContext.jsx
@@ -7,7 +7,15 @@ export const CityProvider = ({ children }) => {
 
   const fetchDataCity = async () => {
     try {
-      const response = await fetch("http://localhost:4000/cities");
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/cities`, {
+        headers: {
+          "Cache-Control": "no-cache",
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
       const data = await response.json();
       setCities(data);
     } catch (error) {
