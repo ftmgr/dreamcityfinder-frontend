5801edeff2305054e10f95fd3b4659574d486cb6
Fatma <fatmagurses1@gmail.com>
2024-04-18 12:49:26 +0200

    Register-login form is centered

--- src/components/AuthenticationForm.jsx
+++ src/components/AuthenticationForm.jsx
@@ -3,6 +3,7 @@ import { useForm } from "@mantine/form";
 import { useNavigate } from "react-router-dom";
 import { useState, useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
+import classes from "../styles/AuthenticationTitle.module.css";
 
 import {
     TextInput,
@@ -60,75 +61,77 @@ export function AuthenticationForm(props) {
     };
 
     return (
-        <Paper
-            radius="md"
-            p="xl"
-            style={{ width: "400px", align: "center" }}
-            withBorder
-            {...props}
-        >
-            <Text size="lg" fw={500}>
-                Welcome! {type === "login" ? <span>Login</span> : <span>Register</span>}
-            </Text>
+        <div className={classes.loginDiv}>
+            <Paper
+                radius="md"
+                p="xl"
 
-            <form onSubmit={(e) => handleCreateUser(e)}>
-                <Stack>
-                    {type === "register" && (
+                withBorder
+                {...props}
+            >
+                <Text size="lg" fw={500}>
+                    Welcome! {type === "login" ? <span>Login</span> : <span>Register</span>}
+                </Text>
+
+                <form onSubmit={(e) => handleCreateUser(e)} >
+                    <Stack>
+                        {type === "register" && (
+                            <TextInput
+                                label="Name"
+                                placeholder="Your name"
+                                value={name}
+                                onChange={(event) => setName(event.currentTarget.value)}
+                                radius="md"
+                            />
+                        )}
+
                         <TextInput
-                            label="Name"
-                            placeholder="Your name"
-                            value={name}
-                            onChange={(event) => setName(event.currentTarget.value)}
+                            required
+                            label="Email"
+                            placeholder="hello@mantine.dev"
+                            value={email}
+                            onChange={(event) => setEmail(event.currentTarget.value)}
+                            // error={email && "Invalid email"}
                             radius="md"
                         />
-                    )}
 
-                    <TextInput
-                        required
-                        label="Email"
-                        placeholder="hello@mantine.dev"
-                        value={email}
-                        onChange={(event) => setEmail(event.currentTarget.value)}
-                        // error={email && "Invalid email"}
-                        radius="md"
-                    />
-
-                    <PasswordInput
-                        required
-                        label="Password"
-                        placeholder="Your password"
-                        value={password}
-                        onChange={(event) => setPassword(event.currentTarget.value)}
-                        // error={password && "Password should include at least 6 characters"}
-                        radius="md"
-                    />
-
-                    {type === "register" && (
-                        <Checkbox
-                            label="I accept terms and conditions"
-                            checked={terms}
-                            onChange={(event) => setTerms(event.currentTarget.checked)}
+                        <PasswordInput
+                            required
+                            label="Password"
+                            placeholder="Your password"
+                            value={password}
+                            onChange={(event) => setPassword(event.currentTarget.value)}
+                            // error={password && "Password should include at least 6 characters"}
+                            radius="md"
                         />
-                    )}
-                </Stack>
 
-                <Group justify="space-between" mt="xl">
-                    <Anchor
-                        component="button"
-                        type="button"
-                        c="dimmed"
-                        onClick={() => toggle()}
-                        size="xs"
-                    >
-                        {type === "register"
-                            ? "Already have an account? Login"
-                            : "Don't have an account? Register"}
-                    </Anchor>
-                    <Button type="submit" radius="xl">
-                        {upperFirst(type)}
-                    </Button>
-                </Group>
-            </form>
-        </Paper>
+                        {type === "register" && (
+                            <Checkbox
+                                label="I accept terms and conditions"
+                                checked={terms}
+                                onChange={(event) => setTerms(event.currentTarget.checked)}
+                            />
+                        )}
+                    </Stack>
+
+                    <Group justify="space-between" mt="xl">
+                        <Anchor
+                            component="button"
+                            type="button"
+                            c="dimmed"
+                            onClick={() => toggle()}
+                            size="xs"
+                        >
+                            {type === "register"
+                                ? "Already have an account? Login"
+                                : "Don't have an account? Register"}
+                        </Anchor>
+                        <Button type="submit" radius="xl">
+                            {upperFirst(type)}
+                        </Button>
+                    </Group>
+                </form>
+            </Paper>
+        </div>
     );
 }
