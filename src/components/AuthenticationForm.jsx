c42b2075c60b8953d6fb79929ec41eadf48b0e6c
Fabio Niglio <fabio_niglio@hotmail.com>
2024-04-18 10:57:21 +0200

    fixing p nesting

--- src/components/AuthenticationForm.jsx
+++ src/components/AuthenticationForm.jsx
@@ -1,22 +1,22 @@
-import { useToggle, upperFirst } from '@mantine/hooks';
-import { useForm } from '@mantine/form';
+import { useToggle, upperFirst } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
 import { useNavigate } from "react-router-dom";
 import { useState, useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 
 import {
-    TextInput,
-    PasswordInput,
-    Text,
-    Paper,
-    Group,
-    Button,
-    Divider,
-    Checkbox,
-    Anchor,
-    Stack,
-    Flex,
-} from '@mantine/core';
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  Button,
+  Divider,
+  Checkbox,
+  Anchor,
+  Stack,
+  Flex,
+} from "@mantine/core";
 
 /*
    {
@@ -32,93 +32,103 @@ import {
             "createdDate": "yyyy-mm-dd hh:mm:ss"
         }*/
 
-
 export function AuthenticationForm(props) {
-    const [type, toggle] = useToggle(['login', 'register']);
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [terms, setTerms] = useState(false);
-    const [favoriteCities, setFavoriteCities] = useState([]);
-    const [createdDate, setCreatedDate] = useState("");
+  const [type, toggle] = useToggle(["login", "register"]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [terms, setTerms] = useState(false);
+  const [favoriteCities, setFavoriteCities] = useState([]);
+  const [createdDate, setCreatedDate] = useState("");
 
-    const navigate = useNavigate();
-    const { addUser } = useContext(UserContext);  //addUser, UserContext? 
+  const navigate = useNavigate();
+  const { addUser } = useContext(UserContext); //addUser, UserContext?
 
-    // Function to handle form submission
-    const handleCreateUser = () => {
-        //setFavoriteCities("Istanbul");
-        //setCreatedDate("04/18/2024");
+  // Function to handle form submission
+  const handleCreateUser = () => {
+    //setFavoriteCities("Istanbul");
+    //setCreatedDate("04/18/2024");
 
-        const newUser = {
-            name: name,
-            email: email,
-            password: password,
-            terms: terms,
-        };
-        addUser(newUser);
-        navigate("/");
+    const newUser = {
+      name: name,
+      email: email,
+      password: password,
+      terms: terms,
     };
+    addUser(newUser);
+    navigate("/");
+  };
 
-    return (
-        <Paper radius="md" p="xl" style={{ width: "400px", align: "center" }} withBorder {...props}>
-            <Text size="lg" fw={500}>
-                Welcome! {type === 'login' ? <p>Login</p> : <p>Register</p>}
+  return (
+    <Paper
+      radius="md"
+      p="xl"
+      style={{ width: "400px", align: "center" }}
+      withBorder
+      {...props}
+    >
+      <Text size="lg" fw={500}>
+        Welcome! {type === "login" ? <span>Login</span> : <span>Register</span>}
+      </Text>
 
-            </Text>
+      <form onSubmit={(e) => handleCreateUser(e)}>
+        <Stack>
+          {type === "register" && (
+            <TextInput
+              label="Name"
+              placeholder="Your name"
+              value={name}
+              onChange={(event) => setName(event.currentTarget.value)}
+              radius="md"
+            />
+          )}
 
-            <form onSubmit={(e) => handleCreateUser(e)}>
-                <Stack>
-                    {type === 'register' && (
-                        <TextInput
-                            label="Name"
-                            placeholder="Your name"
-                            value={name}
-                            onChange={(event) => setName(event.currentTarget.value)}
-                            radius="md"
-                        />
-                    )}
+          <TextInput
+            required
+            label="Email"
+            placeholder="hello@mantine.dev"
+            value={email}
+            onChange={(event) => setEmail(event.currentTarget.value)}
+            error={email && "Invalid email"}
+            radius="md"
+          />
 
-                    <TextInput
-                        required
-                        label="Email"
-                        placeholder="hello@mantine.dev"
-                        value={email}
-                        onChange={(event) => setEmail(event.currentTarget.value)}
-                        error={form.errors.email && 'Invalid email'}
-                        radius="md"
-                    />
+          <PasswordInput
+            required
+            label="Password"
+            placeholder="Your password"
+            value={password}
+            onChange={(event) => setPassword(event.currentTarget.value)}
+            error={password && "Password should include at least 6 characters"}
+            radius="md"
+          />
 
-                    <PasswordInput
-                        required
-                        label="Password"
-                        placeholder="Your password"
-                        value={password}
-                        onChange={(event) => setPassword(event.currentTarget.value)}
-                        error={form.errors.password && 'Password should include at least 6 characters'}
-                        radius="md"
-                    />
+          {type === "register" && (
+            <Checkbox
+              label="I accept terms and conditions"
+              checked={terms}
+              onChange={(event) => setTerms(event.currentTarget.checked)}
+            />
+          )}
+        </Stack>
 
-                    {type === 'register' && (
-                        <Checkbox
-                            label="I accept terms and conditions"
-                            checked={terms}
-                            onChange={(event) => setTerms(event.currentTarget.checked)}
-                        />
-                    )}
-                </Stack>
-
-                <Group justify="space-between" mt="xl">
-                    <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
-                        {type === 'register'
-                            ? 'Already have an account? Login'
-                            : "Don't have an account? Register"}
-                    </Anchor>
-                    <Button type="submit" radius="xl" >
-                        {upperFirst(type)}
-                    </Button>
-                </Group>
-            </form>
-        </Paper>
-    );
+        <Group justify="space-between" mt="xl">
+          <Anchor
+            component="button"
+            type="button"
+            c="dimmed"
+            onClick={() => toggle()}
+            size="xs"
+          >
+            {type === "register"
+              ? "Already have an account? Login"
+              : "Don't have an account? Register"}
+          </Anchor>
+          <Button type="submit" radius="xl">
+            {upperFirst(type)}
+          </Button>
+        </Group>
+      </form>
+    </Paper>
+  );
 }
